f0d200b3ebe6a533d1a5f4fcfb4129fa4697e47b
ayushvrma <ayush8402@gmail.com>
2021-12-04 00:23:44 +0530

    implemented softmax function

---
+++ Introduction/Softmax/softmax.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import math
+
+
+def softmax(X):
+    l1 = []
+    total = 0
+    for i in range(len(X)):
+        total += math.exp(X[i])
+    for i in range(len(X)):
+        l1.append(math.exp(X[i])/total)
+    return l1
+
+def main():
+    X = [4,3,2,1,0,-1,-2,-3]
+    score_to_prob = softmax(X)
+    print(score_to_prob)
+
+
+if __name__ == "__main__":
+    main()
